6cf209e6620ccf7577eb10ffc432c96dcb02e3f1
Vaibhav vats <76244950+vaibhavvatsbhartiya@users.noreply.github.com>
2023-02-25 18:38:11 +0530

    Input-Output in Java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Hello{
+    public static void main(String args[]){
+        // take input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        // next humeasha ik he tocken leta hai so agr m input m
+        //  vaibhav vats du toh bhi bs yeh vaibhav return he dega
+        // to over come this problem
+        // use nextline()
+        System.out.println(name);
+    }
+} 
